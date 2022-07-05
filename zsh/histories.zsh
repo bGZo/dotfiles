0be92e4bb53e13ffd73dce98dd86d9292a8b7b0a
bGZoCg <bgzocg@outlook.com>
2022-07-05 18:42:21 +0800

    style: marge local and remote

--- zsh/histories.zsh
+++ zsh/histories.zsh
@@ -11,8 +11,8 @@ HISTCONTROL=ignorespace
 
 setopt BANG_HIST                 # Treat the '!' character specially during expansion.
 unsetopt EXTENDED_HISTORY        # Write the history file in the ":start:elapsed;command" format.
-unsetopt INC_APPEND_HISTORY      # Write to the history file immediately, not when the shell exits.
-unsetopt SHARE_HISTORY           # Share history between all sessions.
+unsetopt SHARE_HISTORY             # Share history between all sessions.
+setopt INC_APPEND_HISTORY      # Write to the history file immediately, not when the shell exits.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
 setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
 setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
